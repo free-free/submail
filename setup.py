81407c4b26ad2ca1518b1ee831ef9ce708404f17
John <19941222hb@gmail.com>
2016-08-15 23:39:11 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+from setuptools import setup,find_packages
+import re
+
+setup(
+    name="tornado_session",
+    version="0.1",
+    author="HuangBiao",
+    author_email="19941222hb@gmail.com",
+    description="submail sdk",
+    license="MIT",
+    package=find_packages(),
+    install_requires=re.split(r"\r*\n+", open("requirements.txt", 'r').read())[0:-1],
+    include_package_data=True
+)
