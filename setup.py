d8389968013fd11168885a46fc8e38dc49c85f38
John <19941222hb@gmail.com>
2016-08-16 23:47:03 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 # coding:utf-8
 from setuptools import setup,find_packages
 import re
+from submail import __version__
 
 setup(
     name="submail",
-    version="0.1",
+    version=__version__,
     author="HuangBiao",
     author_email="19941222hb@gmail.com",
     description="submail sdk",
     license="MIT",
-    package=find_packages(),
+    packages=find_packages(),
     install_requires=re.split(r"\r*\n+", open("requirements.txt", 'r').read())[0:-1],
     include_package_data=True
 )
