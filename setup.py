0263c5a9699faef43daf6e8184dceb51f7c8e484
John <19941222hb@gmail.com>
2016-08-18 19:36:54 +0800

    add classifiers to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # coding:utf-8
+
 from setuptools import setup,find_packages
 import re
 from submail import __version__
@@ -9,8 +10,19 @@ setup(
     author="HuangBiao",
     author_email="19941222hb@gmail.com",
     description="submail sdk",
+    url="https://github.com/free-free/submail",
     license="MIT",
     packages=find_packages(),
     install_requires=re.split(r"\r*\n+", open("requirements.txt", 'r').read())[0:-1],
-    include_package_data=True
+    include_package_data=True,
+    keywords=['submail','sdk'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
